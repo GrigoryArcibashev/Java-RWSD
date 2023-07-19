8a2428cbf2a3f37f98df7b6d220f4dc37532b9cb
arcibashev71 <arcibashev71@yandex.ru>
2023-07-19 20:33:24 +0500

    chapter_02: BankTransactionCSVParserTest: add shouldParseSeveralCorrectLines

--- src/test/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionCSVParserTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_02/BankTransactionCSVParserTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class  BankTransactionCSVParserTest {
+public class BankTransactionCSVParserTest {
 
     private final BankStatementParser statementParser = new BankStatementCSVParser();
 
@@ -20,6 +23,26 @@ public class  BankTransactionCSVParserTest {
         Assert.assertEquals(expected.getDate(), result.getDate());
         Assert.assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
         Assert.assertEquals(expected.getDescription(), result.getDescription());
+    }
+
+    @Test
+    public void shouldParseSeveralCorrectLines() {
+        String[] lines = {
+                "30-01-2017,-50,Tesco",
+                "28-02-2018,1000,Tesco",
+                "30-03-2019,-250,Tesco"
+        };
+
+        BankTransaction[] result = statementParser
+                .parseLinesFrom(List.of(lines))
+                .toArray(new BankTransaction[0]);
+
+        BankTransaction[] expected = {
+                new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco"),
+                new BankTransaction(LocalDate.of(2018, Month.FEBRUARY, 28), 1000, "Tesco"),
+                new BankTransaction(LocalDate.of(2019, Month.MARCH, 30), -250, "Tesco"),
+        };
+        Assert.assertArrayEquals(expected, result);
     }
 
 }
