34265f4bd248d73e7f9076cc5bbea4146e92ed7d
arcibashev71 <arcibashev71@yandex.ru>
2023-07-19 20:46:10 +0500

    chapter_02: BankStatementProcessorTest: delete simpleTest, add getLeastExpensiveBankTransaction_SimpleTest and getMostExpensiveBankTransaction_SimpleTest

--- src/test/java/com/iteratrlearning/shu_book/chapter_02/BankStatementProcessorTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_02/BankStatementProcessorTest.java
@@ -1,5 +1,6 @@
 package com.iteratrlearning.shu_book.chapter_02;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -8,25 +9,43 @@ import java.util.List;
 import java.util.Optional;
 
 public class BankStatementProcessorTest {
+    private final BankTransaction[] bankTransactions = {
+            new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Test"),
+            new BankTransaction(LocalDate.of(2018, Month.JANUARY, 30), -200, "LEAST"),
+            new BankTransaction(LocalDate.of(2019, Month.JANUARY, 30), -100, "Test"),
+            new BankTransaction(LocalDate.of(2020, Month.JANUARY, 30), 500, "MOST"),
+            new BankTransaction(LocalDate.of(2021, Month.JANUARY, 30), 200, "Test"),
+            new BankTransaction(LocalDate.of(2022, Month.JANUARY, 30), 50, "Test"),
+            new BankTransaction(LocalDate.of(2023, Month.JANUARY, 30), -50, "Test"),
+    };
+    private final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(List.of(bankTransactions));
+
     @Test
-    public void simpleTest() {
-        BankTransaction[] bankTransactions = {
-                new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Test"),
-                new BankTransaction(LocalDate.of(2018, Month.JANUARY, 30), -200, "Lest"),
-                new BankTransaction(LocalDate.of(2019, Month.JANUARY, 30), -100, "Test"),
-                new BankTransaction(LocalDate.of(2020, Month.JANUARY, 30), 500, "Most"),
-                new BankTransaction(LocalDate.of(2021, Month.JANUARY, 30), 200, "Test"),
-                new BankTransaction(LocalDate.of(2022, Month.JANUARY, 30), 50, "Test"),
-                new BankTransaction(LocalDate.of(2023, Month.JANUARY, 30), -50, "Test"),
-        };
-        BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(List.of(bankTransactions));
+    public void getLeastExpensiveBankTransaction_SimpleTest() {
         LocalDate startPeriod = LocalDate.of(2018, Month.JANUARY, 1);
         LocalDate endPeriod = LocalDate.of(2023, Month.JANUARY, 1);
+        BankTransaction expectedLeastExpensiveBankTransaction =
+                new BankTransaction(LocalDate.of(2018, Month.JANUARY, 30), -200, "LEAST");
 
-        Optional<BankTransaction> leastExpensiveBankTransaction =
+        Optional<BankTransaction> actualLeastExpensiveBankTransaction =
                 bankStatementProcessor.getLeastExpensiveBankTransaction(startPeriod, endPeriod);
-        Optional<BankTransaction> mostExpensiveBankTransaction =
-                bankStatementProcessor.getMostExpensiveBankTransaction(startPeriod, endPeriod);
+        bankStatementProcessor.getMostExpensiveBankTransaction(startPeriod, endPeriod);
+        Assert.assertTrue(actualLeastExpensiveBankTransaction.isPresent());
+        Assert.assertEquals(expectedLeastExpensiveBankTransaction, actualLeastExpensiveBankTransaction.get());
+    }
 
+//    public void getLeastExpensiveBankTransaction
+
+    @Test
+    public void getMostExpensiveBankTransaction_SimpleTest() {
+        LocalDate startPeriod = LocalDate.of(2018, Month.JANUARY, 1);
+        LocalDate endPeriod = LocalDate.of(2023, Month.JANUARY, 1);
+        BankTransaction expectedMostExpensiveBankTransaction =
+                new BankTransaction(LocalDate.of(2020, Month.JANUARY, 30), 500, "MOST");
+
+        Optional<BankTransaction> actualMostExpensiveBankTransaction =
+                bankStatementProcessor.getMostExpensiveBankTransaction(startPeriod, endPeriod);
+        Assert.assertTrue(actualMostExpensiveBankTransaction.isPresent());
+        Assert.assertEquals(expectedMostExpensiveBankTransaction, actualMostExpensiveBankTransaction.get());
     }
 }
