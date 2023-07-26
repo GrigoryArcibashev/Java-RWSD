15c9293a53dc67854e4e593046980ea0806a7334
arcibashev71 <arcibashev71@yandex.ru>
2023-07-26 17:39:25 +0500

    tests: тесты из старой версии перенесены в новую

--- src/test/java/com/iteratrlearning/shu_book/chapter_03/BankStatementProcessorTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_03/BankStatementProcessorTest.java
@@ -1,16 +1,75 @@
 package com.iteratrlearning.shu_book.chapter_03;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
 public class BankStatementProcessorTest {
+    private final com.iteratrlearning.shu_book.chapter_02.BankTransaction[] bankTransactions = {
+            new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Test"),
+            new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2018, Month.JANUARY, 30), -200, "LEAST"),
+            new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2019, Month.JANUARY, 30), -100, "Test"),
+            new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2020, Month.JANUARY, 30), 500, "MOST"),
+            new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2021, Month.JANUARY, 30), 200, "Test"),
+            new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2022, Month.JANUARY, 30), 50, "Test"),
+            new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2023, Month.JANUARY, 30), -50, "Test"),
+    };
+    private final com.iteratrlearning.shu_book.chapter_02.BankStatementProcessor bankStatementProcessor = new com.iteratrlearning.shu_book.chapter_02.BankStatementProcessor(List.of(bankTransactions));
+
+    @Test
+    public void getLeastExpensiveBankTransaction_simpleTest() {
+        LocalDate startPeriod = LocalDate.of(2018, Month.JANUARY, 1);
+        LocalDate endPeriod = LocalDate.of(2023, Month.JANUARY, 1);
+        com.iteratrlearning.shu_book.chapter_02.BankTransaction expectedLeastExpensiveBankTransaction =
+                new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2018, Month.JANUARY, 30), -200, "LEAST");
+
+        Optional<com.iteratrlearning.shu_book.chapter_02.BankTransaction> actualLeastExpensiveBankTransaction =
+                bankStatementProcessor.getLeastExpensiveBankTransaction(startPeriod, endPeriod);
+        bankStatementProcessor.getMostExpensiveBankTransaction(startPeriod, endPeriod);
+        Assert.assertTrue(actualLeastExpensiveBankTransaction.isPresent());
+        Assert.assertEquals(expectedLeastExpensiveBankTransaction, actualLeastExpensiveBankTransaction.get());
+    }
+
+    @Test
+    public void getMostExpensiveBankTransaction_simpleTest() {
+        LocalDate startPeriod = LocalDate.of(2018, Month.JANUARY, 1);
+        LocalDate endPeriod = LocalDate.of(2023, Month.JANUARY, 1);
+        com.iteratrlearning.shu_book.chapter_02.BankTransaction expectedMostExpensiveBankTransaction =
+                new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2020, Month.JANUARY, 30), 500, "MOST");
+
+        Optional<com.iteratrlearning.shu_book.chapter_02.BankTransaction> actualMostExpensiveBankTransaction =
+                bankStatementProcessor.getMostExpensiveBankTransaction(startPeriod, endPeriod);
+        Assert.assertTrue(actualMostExpensiveBankTransaction.isPresent());
+        Assert.assertEquals(expectedMostExpensiveBankTransaction, actualMostExpensiveBankTransaction.get());
+    }
+
+    @Test
+    public void getLeastExpensiveBankTransaction_shouldNotGetAnything() {
+        LocalDate startPeriod = LocalDate.of(2010, Month.JANUARY, 1);
+        LocalDate endPeriod = LocalDate.of(2014, Month.JANUARY, 1);
+
+        Optional<com.iteratrlearning.shu_book.chapter_02.BankTransaction> actualLeastExpensiveBankTransaction =
+                bankStatementProcessor.getLeastExpensiveBankTransaction(startPeriod, endPeriod);
+        Assert.assertFalse(actualLeastExpensiveBankTransaction.isPresent());
+    }
+
+    @Test
+    public void getMostExpensiveBankTransaction_shouldNotGetAnything() {
+        LocalDate startPeriod = LocalDate.of(2010, Month.JANUARY, 1);
+        LocalDate endPeriod = LocalDate.of(2014, Month.JANUARY, 1);
+
+        Optional<com.iteratrlearning.shu_book.chapter_02.BankTransaction> actualMostExpensiveBankTransaction =
+                bankStatementProcessor.getMostExpensiveBankTransaction(startPeriod, endPeriod);
+        Assert.assertFalse(actualMostExpensiveBankTransaction.isPresent());
+    }
 
     @Test
     public void shouldFilterTransactionsInFebruary() {
@@ -23,7 +82,7 @@ public class BankStatementProcessorTest {
 
         final List<BankTransaction> bankTransactions
                 = List.of(februarySalary,
-                          marchRoyalties);
+                marchRoyalties);
 
         final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
         final List<BankTransaction> transactions
--- src/test/java/com/iteratrlearning/shu_book/chapter_03/BankTransactionCSVParserTest.java
+++ src/test/java/com/iteratrlearning/shu_book/chapter_03/BankTransactionCSVParserTest.java
@@ -1,5 +1,8 @@
-package com.iteratrlearning.shu_book.chapter_02;
+package com.iteratrlearning.shu_book.chapter_03;
 
+import com.iteratrlearning.shu_book.chapter_02.BankStatementCSVParser;
+import com.iteratrlearning.shu_book.chapter_02.BankStatementParser;
+import com.iteratrlearning.shu_book.chapter_02.BankTransaction;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,9 +20,9 @@ public class BankTransactionCSVParserTest {
     public void shouldParseOneCorrectLine() {
         String line = "30-01-2017,-50,Tesco";
 
-        BankTransaction result = statementParser.parseFrom(line);
+        com.iteratrlearning.shu_book.chapter_02.BankTransaction result = statementParser.parseFrom(line);
 
-        BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+        com.iteratrlearning.shu_book.chapter_02.BankTransaction expected = new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
         Assert.assertEquals(expected.getDate(), result.getDate());
         Assert.assertEquals(expected.getAmount(), result.getAmount(), 0.0d);
         Assert.assertEquals(expected.getDescription(), result.getDescription());
@@ -33,13 +36,13 @@ public class BankTransactionCSVParserTest {
                 "30-03-2019,-250,Tesco"
         };
 
-        BankTransaction[] result = statementParser
+        com.iteratrlearning.shu_book.chapter_02.BankTransaction[] result = statementParser
                 .parseLinesFrom(List.of(lines))
-                .toArray(new BankTransaction[0]);
+                .toArray(new com.iteratrlearning.shu_book.chapter_02.BankTransaction[0]);
 
-        BankTransaction[] expected = {
-                new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco"),
-                new BankTransaction(LocalDate.of(2018, Month.FEBRUARY, 28), 1000, "Tesco"),
+        com.iteratrlearning.shu_book.chapter_02.BankTransaction[] expected = {
+                new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco"),
+                new com.iteratrlearning.shu_book.chapter_02.BankTransaction(LocalDate.of(2018, Month.FEBRUARY, 28), 1000, "Tesco"),
                 new BankTransaction(LocalDate.of(2019, Month.MARCH, 30), -250, "Tesco"),
         };
         Assert.assertArrayEquals(expected, result);
