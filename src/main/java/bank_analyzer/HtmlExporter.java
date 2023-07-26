ef1c6d5f7acf383afadc2b064ea7ae89cc151a5c
arcibashev71 <arcibashev71@yandex.ru>
2023-07-26 19:17:34 +0500

    feat(app): улучшить генерацию отчета в html

--- src/main/java/bank_analyzer/HtmlExporter.java
+++ src/main/java/bank_analyzer/HtmlExporter.java
@@ -3,18 +3,18 @@ package bank_analyzer;
 public class HtmlExporter implements Exporter {
     @Override
     public String export(final SummaryStatistics summaryStatistics) {
-        String result = "<!DOCTYPE html>";
-        result += "<html lang='en'>";
-        result += "<head><title>Bank Transaction Report</title></head>";
-        result += "<body>";
-        result += "<ul>";
-        result += "<li><strong>The sum is</strong>: " + summaryStatistics.getSum() + "</li>";
-        result += "<li><strong>The average is</strong>: " + summaryStatistics.getAverage() + "</li>";
-        result += "<li><strong>The max is</strong>: " + summaryStatistics.getMax() + "</li>";
-        result += "<li><strong>The min is</strong>: " + summaryStatistics.getMin() + "</li>";
-        result += "</ul>";
-        result += "</body>";
-        result += "</html>";
+        String result = "<!DOCTYPE html>\n";
+        result += "<html lang='en'>\n";
+        result += "<head><title>Bank Transaction Report</title></head>\n";
+        result += "<body>\n";
+        result += "<ul>\n";
+        result += "\t<li><strong>The sum is</strong>: " + summaryStatistics.getSum() + "</li>\n";
+        result += "\t<li><strong>The average is</strong>: " + summaryStatistics.getAverage() + "</li>\n";
+        result += "\t<li><strong>The max is</strong>: " + summaryStatistics.getMax() + "</li>\n";
+        result += "\t<li><strong>The min is</strong>: " + summaryStatistics.getMin() + "</li>\n";
+        result += "</ul>\n";
+        result += "</body>\n";
+        result += "</html>\n";
         return result;
     }
 }
