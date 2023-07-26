29bd55ec87fca20401241d9ebcbebfa688a6043e
arcibashev71 <arcibashev71@yandex.ru>
2023-07-26 19:21:10 +0500

    comment: добавить TODO
    TODO вынести чтение и запись данных в отдельные сущности (OCP!)

--- src/main/java/bank_analyzer/MainApplication.java
+++ src/main/java/bank_analyzer/MainApplication.java
@@ -1,10 +1,17 @@
 package bank_analyzer;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MainApplication {
     public static void main(final String[] args) throws Exception {
         final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
         final BankStatementParser bankStatementParser = new BankStatementCSVParser();
         final Exporter exporter = new HtmlExporter();
+        //TODO вынести чтение и запись данных в отдельные сущности (OCP!)
         String report = bankStatementAnalyzer.analyze("bank-data-simple.csv", bankStatementParser, exporter);
+        final Path path = Paths.get("src/main/resources/index.html");
+        Files.write(path, report.getBytes());
     }
 }
